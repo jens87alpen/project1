e0719c19aa803d4d1fbbfbac550449a8f2935cf5
Tom Hughes <tomkhughes@g.cofc.edu>
2018-01-06 13:40:20 -0800

    map without search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,45 @@
-document.addEventListener('DOMContentLoaded', bind);
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
 
-function bind() {
-    var el = document.getElementById('beerSub');
-    el.addEventListener('click', function(event) {
-        var base = "http://api.brewerydb.com/v2"
-        var beerName = document.getElementById('br').value;
-        var bts = beerName.split(' ').join('%20');
-        var type = "&type=beer";
-        var key = "&key=2e10788536db6bc8ca751b72291ba1dc";
-        var search = "/search?q=";
-        var fullUrl = base + search + bts + type + key;
-        var req = new XMLHttpRequest();
-        req.open("GET", fullUrl, true);
+var beer = $(this).attr("data-name");
+var proxyURL = "https://desolate-tundra-96867.herokuapp.com/"
+var queryURL = proxyURL + "http://api.brewerydb.com/v2/beers?styleId=30&withBreweries=Y&key=2e10788536db6bc8ca751b72291ba1dc";
 
-        req.send(null);
-        req.addEventListener('load', function() {
-            var response = JSON.parse(req.responseText);
-            document.getElementById("sb").textContent = response.data[0].name;
-            document.getElementById("id").textContent = response.data[0].id;
-            //We begin making the new request here, inside the first!
-            var beer = /beer/;
-            var brew = "?withBreweries=y"
-            var id = response.data[0].id;
-            var newUrl = base + beer + id + brew + key
-            var req2 = new XMLHttpRequest();
-            req2.open("GET", newUrl, true);
-            req2.send(null);
-            req2.addEventListener('load', function() {
-                var response2 = JSON.parse(req2.responseText); //parse the response tex
-                console.log(response2);
-                document.getElementById("brew").textContent = response2.data.breweries[0].name;
-                var street = response2.data.breweries[0].locations[0].streetAddress;
-                var city = response2.data.breweries[0].locations[0].locality;
-                var state = response2.data.breweries[0].locations[0].region;
-                var zip = response2.data.breweries[0].locations[0].postalCode;
-                var fullAddr = street + ', ' + city + ', ' + state + ' ' + zip;
-                document.getElementById("loc").textContent = fullAddr;
-                document.getElementById("brewImg").src = response2.data.breweries[0].images.large;
+$.ajax({
+         url: queryURL,
+         method: "GET"
+     })
+    
+    .done(function(response) {
+       console.log(response);
+       var results = response.data;
+       for (var i = 0; i < results.length; i++) {
+        var breweryName = results[i].breweries[0].name;
+        var beerName = results[i].name;
+        var latitude = results[i].breweries[0].locations[0].latitude;
+        var logitude = results[i].breweries[0].locations[0].longitude;
+        console.log(results[i].name)
+       }     
+});
 
-            });
+// .done(function(response) {
+//       console.log(response);
+//       var results = response.data;
+//       for (var i = 0; i < results.length; i++) {
+//         var gifDiv = $("<div class='item'>");
+//         var rating =results[i].rating;
+//         var paragraph = $('<p>').text("Rating: " + rating);
+//         var personImage=$('<img>');
+//         personImage.attr("class","gif");
+//             personImage.attr("src", results[i].images.fixed_height_still.url);
+//             personImage.attr("data-state","still")
+//             personImage.attr("data-still", results[i].images.fixed_height_still.url);
+//             personImage.attr("data-animate", results[i].images.fixed_height.url);
+//             gifDiv.prepend(paragraph);
+//             gifDiv.prepend(personImage);
 
-        });
-        event.preventDefault();
-    });
-};
+//         $(".athlete-giph").prepend(gifDiv);
+//       }
+//     })
+// })
